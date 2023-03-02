c2ccdfa5ad11f8ec0550f327c6a7d104e2b8901a
u0733172 <u0733172@users.noreply.github.com>
2023-03-02 15:14:30 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -116,7 +116,7 @@ def main():
     st.sidebar.header("**General Info**")
 
     # Loading selectbox
-    chk_id = st.sidebar.selectbox("Client ID", id_client)
+         chk_id = st.sidebar.selectbox("Client ID", id_client)
 
     # Loading general info
     nb_credits, rev_moy, credits_moy, targets = load_infos_gen(data)
